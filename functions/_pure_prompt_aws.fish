4cb617f6a4f1e8eb13c8e0a1af2b558fa90bb5e3
Scott Miller <scott@millergeek.xyz>
2019-03-14 16:09:14 -0400

    fix prompt

--- functions/_pure_prompt_aws.fish
+++ functions/_pure_prompt_aws.fish
@@ -7,5 +7,5 @@ function _pure_prompt_aws \
     set --local vimode_indicator (_pure_prompt_vimode) # vi-mode indicator
     set --local pure_symbol (_pure_prompt_symbol $exit_code)
 
-    echo (_pure_print_prompt $virtualenv $vimode_indicator $pure_symbol)
+    echo (_pure_print_prompt $virtualenv $awsenv $vimode_indicator $pure_symbol)
 end
