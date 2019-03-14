1de6cf8a4bb89a0f6313bea9cc0bb2eacf232c4d
Scott Miller <scott@millergeek.xyz>
2019-03-14 16:23:51 -0400

    remove extras

--- functions/_okta_aws_prompt.fish
+++ functions/_okta_aws_prompt.fish
@@ -1,4 +1,4 @@
-function _pure_prompt_awsenv --description "Display AWS Okta Profile"
+function _okta_aws_prompt --description "Display AWS Okta Profile"
     if test -n "$AWS_OKTA_PROFILE"
         set --local awsenv (basename "$AWS_OKTA_PROFILE")
         set --local awsenv_color (set_color yellow)
