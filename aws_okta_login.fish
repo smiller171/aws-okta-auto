736db28585fdac252c2637a60adff6eae215cce4
Scott Miller <scott@millergeek.xyz>
2019-04-19 13:15:51 -0400

    make vars universal

--- aws_okta_login.fish
+++ aws_okta_login.fish
@@ -1,3 +1,3 @@
 function aws_okta_login
-  aws-okta exec $argv[1] -- env | grep AWS - | sed 's/^/set -gx /' | sed 's/=/ /' | source
+    aws-okta exec $argv[1] -- env | grep AWS - | sed 's/^/set -Ux /' | sed 's/=/ /' | source
 end
