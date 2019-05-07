8cf2c28e4427ceabb2a6559623cebbf943b63d33
Scott Miller <scott@millergeek.xyz>
2019-05-07 12:11:02 -0400

    don't export vars

--- aws_okta_login.fish
+++ aws_okta_login.fish
@@ -1,3 +1,3 @@
 function aws_okta_login
-    aws-okta exec $argv[1] -- env | grep AWS - | sed 's/^/set -Ux /' | sed 's/=/ /' | source
+    aws-okta exec $argv[1] -- env | grep AWS - | sed 's/^/set -U /' | sed 's/=/ /' | source
 end
